49c07d8b55e3a7141246bff314f1b2e2788d2650
imstan319 <134049324+imstan319@users.noreply.github.com>
2023-06-07 23:20:35 +0800

    sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -119,9 +119,9 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+        <div className={styles["sidebar-title"]}>沉静如海</div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          最好的法语电影之一
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
