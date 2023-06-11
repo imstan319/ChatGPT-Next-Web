cc9a98a9e8da6a1a551fbfb4ed267af71f153f77
imstan319 <134049324+imstan319@users.noreply.github.com>
2023-06-11 22:24:09 +0800

    config.ts

--- app/store/config.ts
+++ app/store/config.ts
@@ -31,11 +31,11 @@ export const DEFAULT_CONFIG = {
 
   modelConfig: {
     model: "gpt-3.5-turbo" as ModelType,
-    temperature: 0.5,
+    temperature: 0.0,
     max_tokens: 2000,
     presence_penalty: 0,
     sendMemory: true,
-    historyMessageCount: 4,
+    historyMessageCount: 32,
     compressMessageLengthThreshold: 1000,
   },
 };
